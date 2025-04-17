54efcf12189fee3c3cc60c56c665579b508e7ce3
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-17 18:24:42 +0200

    Feat: customize shadcn ui basic components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-
-
+"use client";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 export default function Home() {
+  const [input, setInput] = useState("")
   return (
-    <>
-      <h1 className="text-gray-600">Hello</h1>
-    </>);
+    <div className="p-4 flex flex-col gap-y-4 max-w-sm mx-auto">
+
+      <Input value={input} onChange={(e) => setInput(e.target.value)} />
+      <Button variant="elevated">Button</Button>
+      <Progress value={40} />
+      <Textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <Checkbox />
+    </div>
+  );
 }
