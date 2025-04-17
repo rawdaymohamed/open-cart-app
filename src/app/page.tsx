b910cc5d47b051b5e29462ef2a84e2110eddb33e
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-17 17:56:17 +0200

    Feat: Add dm sans font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@/components/ui/button";
+
 
 
 export default function Home() {
   return (
-    <><Button>Hello</Button></>
-  );
+    <>
+      <h1 className="text-gray-600">Hello</h1>
+    </>);
 }
